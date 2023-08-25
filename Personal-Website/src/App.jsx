4050a1883f34c3f89e4805fda7f782517a2bb43c
taiteb <taiteb@gmail.com>
2023-08-25 02:26:42 -0600

    init build- header w dynamic iframe background

---
+++ Personal-Website/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header role='banner'>
+        <h1>Taite McGrady</h1>
+        <iframe src="https://taiteb.github.io/gradientSweep" scrolling='no' ></iframe>
+      </header>
+    </>
+  )
+}
+
+export default App

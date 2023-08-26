5fd6dd2871e2f701b3f7b6ee6c2e60fbf9b3fd09
taiteb <taiteb@gmail.com>
2023-08-25 21:41:27 -0600

    Add routes, basic scaffolding for pages

--- Personal-Website/src/App.jsx
+++ Personal-Website/src/App.jsx
@@ -1,4 +1,9 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Recent from './pages/recent'
+import Portfolio from './pages/portfolio'
+import Contact from './pages/contact'
+import About from './pages/about'
 import './App.css'
 
 function App() {
@@ -8,7 +13,19 @@ function App() {
       <header role='banner'>
         <h1>Taite McGrady</h1>
         <iframe src="https://taiteb.github.io/gradientSweep" scrolling='no' ></iframe>
+        <nav className='links'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/portfolio'>Portfolio</Link>
+          <Link to='/contact'>Contact</Link>
+        </nav>
       </header>
+      <Routes>
+        <Route path='/' element={<Recent />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
     </>
   )
 }
---
+++ Personal-Website/src/pages/Recent.jsx
@@ -0,0 +1,8 @@
+export default function Recent(){
+    return(<>
+        <h1>Recent projects:</h1>
+        <iframe src="https://taiteb.github.io/8-24-23/" frameborder="0"></iframe>
+        <iframe src="https://taiteb.github.io/maze-challenge/" frameborder="0"></iframe>
+        <iframe src="https://taiteb.github.io/Working-with-Agents/" frameborder="0"></iframe>
+        </>)
+}

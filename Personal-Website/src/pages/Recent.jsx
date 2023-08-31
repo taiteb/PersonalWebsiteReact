8f690909bafef1ce48e88586f8572eb1d572f7dd
taiteb <taiteb@gmail.com>
2023-08-31 14:17:10 -0600

    feat: iframe larger

--- Personal-Website/src/pages/Recent.jsx
+++ Personal-Website/src/pages/Recent.jsx
@@ -1,8 +1,8 @@
 export default function Recent(){
-    return(<>
-        <h1>Recent projects:</h1>
+    return(<div className="TextFill">
+        <h2>Recent projects:</h2>
         <iframe src="https://taiteb.github.io/8-24-23/" frameborder="0"></iframe>
         <iframe src="https://taiteb.github.io/maze-challenge/" frameborder="0"></iframe>
         <iframe src="https://taiteb.github.io/Working-with-Agents/" frameborder="0"></iframe>
-        </>)
+        </div>)
 }

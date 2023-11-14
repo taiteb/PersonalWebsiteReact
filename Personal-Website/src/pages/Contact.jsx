89a78303da43a936601ec3f31be96d8edda39747
taiteb <taiteb@gmail.com>
2023-11-14 00:29:03 -0700

    contact cpnt styling change, light styling changes

--- Personal-Website/src/pages/Contact.jsx
+++ Personal-Website/src/pages/Contact.jsx
@@ -4,20 +4,22 @@ export default function Contact() {
         <div className="ContentBox">
             <ul>
                 <li className="Links">
-                    <img className="LinkIcon" src="/mail.svg" alt="Mail icon"></img>
+                    <a>
+                        <img className="LinkIcon" src="/mail.svg" alt="Mail icon"></img>
+                    </a>
                     <p>taitemcg@gmail.com</p>
                 </li>
                 <li className="Links">
                     <a href="https://www.linkedin.com/in/taite-mcgrady/">
                         <img className="LinkIcon" src="/linkedin.svg" alt="LinkedIn icon"></img>
                     </a>
-                    <p>Taite McGrady</p>
+                    <a href="https://www.linkedin.com/in/taite-mcgrady/">Taite McGrady</a>
                 </li>
                 <li className="Links">
                     <a href="https://www.github.com/taiteb">
                         <img className="LinkIcon" src="/github.svg" alt="GitHub icon"></img>
                     </a>
-                    <p>Taiteb</p>
+                    <a href="https://www.github.com/taiteb">Taiteb</a>
                 </li>
             </ul>
 
